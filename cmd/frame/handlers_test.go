65485f991c392815218bd7ec4131b7ded13bc8c7
im7mortal <im7mortal@gmail.com>
2016-09-20 21:36:01 +0300

    it's executable now
    test not passed
    
    all test is passed

--- cmd/frame/handlers_test.go
+++ cmd/frame/handlers_test.go
@@ -7,31 +7,46 @@ import (
 	"net/http/httptest"
 	"net/http"
 	"github.com/stretchr/testify/assert"
+	"os"
+	"flag"
 )
 
-func TestIndex(t *testing.T) {
+var router *gin.Engine
+
+func TestMain(m *testing.M) {
+	flag.Parse()
+
 	// SETUP
 	gin.SetMode(gin.TestMode)
-	r := gin.Default()
+	router = gin.Default()
+
+	os.Exit(m.Run())
+}
+
+
+
+
+
+func TestIndex(t *testing.T) {
 
 	// RUN
-	r.GET("/", handlerIndex)
+	router.GET("/", handlerIndex)
 	req, err := http.NewRequest("GET", "/", nil)
 	if err != nil {
 		fmt.Println(err)
 	}
 	resp := httptest.NewRecorder()
-	r.ServeHTTP(resp, req)
+	router.ServeHTTP(resp, req)
 	assert.Equal(t, resp.Code, 200)
 	assert.Equal(t, resp.Body.Bytes(), []byte("{\"message\":\"Welcome to the plot device.\"}\n"))
 
-	r.POST("/contact", handlerContact)
+	router.POST("/contact", handlerContact)
 	req, err = http.NewRequest("POST", "/contact", nil)
 	if err != nil {
 		fmt.Println(err)
 	}
 	resp = httptest.NewRecorder()
-	r.ServeHTTP(resp, req)
+	router.ServeHTTP(resp, req)
 	assert.Equal(t, resp.Code, 200)
 	assert.Equal(t, resp.Body.Bytes(), []byte("{\"message\":\"Success.\"}\n"))
 
